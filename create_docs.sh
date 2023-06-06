b49b238d12ffc0e0ae13c998da8f721d4bc704d8
Eric Engle <EricEngle-NOAA@users.noreply.github.com>
2023-06-06 10:45:20 -0400

    Initial commit for grib2io-interp package.
    This package was originally a part of the grib2io package.  This
    packages contains a Python interface to the NCEPLIBS-ip library.  Since
    this library is Fortran-based, you must use NumPy's distutil's Extension
    and setup to build and install.
    
    This is led to too many complications with grib2io build/install process
    which uses setuptools.

---
+++ create_docs.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# ---------------------------------------------------------------------------------------- 
+# Uses pdoc (https://github.com/mitmproxy/pdoc)
+# ---------------------------------------------------------------------------------------- 
+sysarch=$(uname -m)
+build_dir=$(find . -name "lib.*${sysarch}*" -type d)
+echo "Building docs from: $build_dir"
+pdoc -o 'docs' $build_dir/grib2io_interp
