e5b690fafc0e87cab4fd4a3861fa7215626cd597
Timothy Cera <timothy.cera@noaa.gov>
2024-01-18 17:21:49 -0500

    Yet another fix...

--- setup.py
+++ setup.py
@@ -11,7 +11,9 @@ VERSION = '1.0.3'
 
 libdirs = []
 incdirs = []
-libraries = ['libsp_4','libip_4']
+libraries = ['sp_4','ip_4']
+if sys.platform == "win32":
+    libraries = ["libsp_4", "libip_4"]
 
 
 # fmt: on
@@ -19,15 +21,17 @@ libraries = ['libsp_4','libip_4']
 # find_library.
 # ----------------------------------------------------------------------------------------
 def find_library(name, dirs=None):
-    _libext_by_platform = {"linux": ".so", "darwin": ".dylib"}
+    _libext_by_platform = {"linux": ".so", "darwin": ".dylib", "win32": ".dll"}
     out = []
     sysinfo = (os.name, sys.platform)
 
     # According to the ctypes documentation Mac and Windows ctypes_find_library
-    # returns the full path.  Also ctypes_find_library doesn't work for macOS
+    # returns the full path.  Also ctypes_find_library returns None for macOS
     # on Apple Silicon.
-    if sysinfo not in [("posix", "linux"), ("darwin", "arm64")]:
-        return ctypes_find_library(name)
+    if sys.platform != "linux":
+        for sname in [ctypes_find_library(name), ctypes_find_library(f"lib{name}")]:
+            if sname is not None:
+                return sname
 
     # For Linux and macOS on Apple Silicon have to search ourselves.
     libext = _libext_by_platform[sys.platform]
@@ -76,13 +80,13 @@ config.read(setup_cfg)
 # ----------------------------------------------------------------------------------------
 if os.environ.get('SP_DIR'):
     sp_dir = os.environ.get('SP_DIR')
-    sp_libdir = os.path.dirname(find_library('libsp_4', dirs=[sp_dir]))
+    sp_libdir = os.path.dirname(find_library('sp_4', dirs=[sp_dir]))
 else:
     sp_dir = config.get('directories','sp_dir',fallback=None)
     if sp_dir is None:
-        sp_libdir = os.path.dirname(find_library('libsp_4'))
+        sp_libdir = os.path.dirname(find_library('sp_4'))
     else:
-        sp_libdir = os.path.dirname(find_library('libsp_4', dirs=[sp_dir]))
+        sp_libdir = os.path.dirname(find_library('sp_4', dirs=[sp_dir]))
 libdirs.append(sp_libdir)
 
 # ----------------------------------------------------------------------------------------
@@ -90,15 +94,15 @@ libdirs.append(sp_libdir)
 # ----------------------------------------------------------------------------------------
 if os.environ.get('IP_DIR'):
     ip_dir = os.environ.get('IP_DIR')
-    ip_libdir = os.path.dirname(find_library('libip_4', dirs=[ip_dir]))
+    ip_libdir = os.path.dirname(find_library('ip_4', dirs=[ip_dir]))
     ip_incdir = os.path.join(ip_dir,'include_4')
 else:
     ip_dir = config.get('directories','ip_dir',fallback=None)
     if ip_dir is None:
-        ip_libdir = os.path.dirname(find_library('libip_4'))
+        ip_libdir = os.path.dirname(find_library('ip_4'))
         ip_incdir = os.path.join(os.path.dirname(ip_libdir),'include_4')
     else:
-        ip_libdir = os.path.dirname(find_library('libip_4', dirs=[ip_dir]))
+        ip_libdir = os.path.dirname(find_library('ip_4', dirs=[ip_dir]))
         ip_incdir = os.path.join(os.path.dirname(ip_libdir),'include_4')
 libdirs.append(ip_libdir)
 incdirs.append(ip_incdir)
