d0d83a7152919fe10a2e1d917382c2c4bc9d6132
Eric Engle <EricEngle-NOAA@users.noreply.github.com>
2024-03-20 11:05:50 -0400

    Update for grib2io_interp init
    Added Python functions for getting and setting OpenMP number of
    threads. These functions call the same-named Fortran functions
    in the openmp_handler F2PY extension module.
    
    Updated docstrings.

--- src/grib2io_interp/__init__.py
+++ src/grib2io_interp/__init__.py
@@ -3,19 +3,53 @@ Introduction
 ============
 
 grib2io_interp is a component package to the parent package, [grib2io](https://github.com/NOAA-MDL/grib2io).
-This package provides interfaces to the [NCEPLIBS-ip](https://github.com/NOAA-EMC/NCEPLIBS-ip) general
-interpolation library.
-
-grib2io_interp provides interfaces for performing scalar and vector interpolation.  `grib2io_interp.interpolate` is
-a f2py module extension that contains 2 subroutines: `interpolate_scalar` and `interpolate_vector`.  These
+This package provides interfaces to the Fortran-based [NCEPLIBS-ip](https://github.com/NOAA-EMC/NCEPLIBS-ip)
+general interpolation library. The interface supports performing scalar and vector interpolation. `grib2io_interp.interpolate`
+is a F2PY extension module that contains 2 subroutines: `interpolate_scalar` and `interpolate_vector`.  These
 subroutines serve as wrappers to the NCEPLIBS-ip subroutines `ipolates_grib2` and `ipolatev_grib2` respectively.
 
 It is **recommended** that you access these interpolation subroutines via [`grib2io.interpolate()`](https://noaa-mdl.github.io/grib2io/grib2io.html#interpolate).
+
+OpenMP Support
+==============
+
+NCEPLIBS-ip can be built with support for OpenMP threading. This is also supported in grib2io-interp. The
+F2PY signatures for `interpolate_scalar` and `interpolate_vector` subroutines contain the keyword `threadsafe`
+which unlocks Python's global interpretor lock (gil) and allows for threading. grib2io-interp contains a
+second F2PY extension module, `grib2io_interp.openmp_handler`, that contains functions `get_openmp_threads()`
+and `set_openmp_threads()`.
 """
 
 from .__config__ import grib2io_interp_version as __version__
 
-try:
-    from .openmp_handler import *
-except(ImportError):
-    pass
+def get_openmp_threads():
+    """
+    Return the number of OpenMP threads.
+
+    Returns
+    -------
+    int
+        The number of OpenMP threads.
+    """
+    try:
+        from . import openmp_handler
+        return openmp_handler.get_openmp_threads()
+    except(ImportError):
+        pass
+
+def set_openmp_threads(n: int):
+    """
+    Set the number of OpenMP threads.
+
+    Parameters
+    ----------
+    n
+        The number of OpenMP threads.
+    """
+    if n < 1:
+        raise ValueError(f'Number of threads must be > 0.')
+    try:
+        from . import openmp_handler
+        openmp_handler.set_openmp_threads(n)
+    except(ImportError):
+        pass
