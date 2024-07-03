0246b68c77ad3f08b1e3476bd2abcb7525f26b78
Timothy Cera <timothy.cera@noaa.gov>
2024-07-02 21:42:21 -0400

    initial meson.build with untested openmp

--- src/grib2io_interp/__init__.py
+++ src/grib2io_interp/__init__.py
@@ -17,12 +17,14 @@ NCEPLIBS-ip can be built with support for OpenMP threading. This is also support
 F2PY signatures for `interpolate_scalar` and `interpolate_vector` subroutines contain the keyword `threadsafe`
 which unlocks Python's global interpretor lock (gil) and allows for threading. grib2io-interp contains a
 second F2PY extension module, `grib2io_interp.openmp_handler`, that allows the user to get and set the number
-of OpenMP threads.  These functions are made public by `grib2io_interp.get_openmp_threads()` and 
+of OpenMP threads.  These functions are made public by `grib2io_interp.get_openmp_threads()` and
 `grib2io_interp.set_openmp_threads()`.
 """
 
-from .__config__ import grib2io_interp_version as __version__
-from .__config__ import has_openmp_support
+import importlib
+
+__version__ = importlib.metadata.version('grib2io_interp')
+
 
 def get_openmp_threads():
     """
--- src/interpolate/interpolate.f90
+++ src/interpolate/interpolate.f90
@@ -1,98 +1,104 @@
-subroutine interpolate_scalar(ip,ipopt,igdtnumi,igdtmpli,igdtleni,igdtnumo,igdtmplo,&
-                              igdtleno,mi,mo,km,ibi,li,gi,no,ibo,lo,go,rlat,rlon,iret)
-use ipolates_mod
-implicit none
+subroutine interpolate_scalar(ip, ipopt, igdtnumi, igdtmpli, igdtleni, &
+                              igdtnumo, igdtmplo, igdtleno, mi, mo, km, ibi, &
+                              li, gi, no, ibo, lo, go, rlat, rlon, iret)
+   !f2py threadsafe
+   use ipolates_mod
+   implicit none
 
-! ---------------------------------------------------------------------------------------- 
-! Input/Output Variables
-! ---------------------------------------------------------------------------------------- 
-integer, intent(in) :: ip
-integer, intent(in), dimension(20) :: ipopt
-integer, intent(in) :: igdtnumi
-integer, intent(in), dimension(igdtleni) :: igdtmpli
-integer, intent(in) :: igdtleni
-integer, intent(in) :: igdtnumo
-integer, intent(in), dimension(igdtleno) :: igdtmplo
-integer, intent(in) :: igdtleno
-integer, intent(in) :: mi
-integer, intent(in) :: mo
-integer, intent(in) :: km
-integer, intent(in), dimension(km) :: ibi
-logical(kind=1), intent(in), dimension(mi,km) :: li
-real, intent(in), dimension(mi,km) :: gi
-integer, intent(out) :: no 
-integer, intent(out), dimension(km) :: ibo
-logical(kind=1), intent(out), dimension(mo,km) :: lo
-real, intent(out), dimension(mo,km) :: go
-real, intent(inout), dimension(mo) :: rlat
-real, intent(inout), dimension(mo) :: rlon
-integer, intent(out) :: iret 
+   ! --------------------------------------------------------------------------
+   ! Input/Output Variables
+   ! --------------------------------------------------------------------------
+   integer, intent(in) :: ip
+   integer, intent(in), dimension(20) :: ipopt
+   integer, intent(in) :: igdtnumi
+   integer, intent(in) :: igdtleni
+   integer, intent(in), dimension(igdtleni) :: igdtmpli
+   integer, intent(in) :: igdtnumo
+   integer, intent(in) :: igdtleno
+   integer, intent(in), dimension(igdtleno) :: igdtmplo
+   integer, intent(in) :: mi
+   integer, intent(in) :: mo
+   integer, intent(in) :: km
+   integer, intent(in), dimension(km) :: ibi
+   logical(kind=1), intent(in), dimension(mi, km) :: li
+   real, intent(in), dimension(mi, km) :: gi
+   integer, intent(out) :: no
+   integer, intent(out), dimension(km) :: ibo
+   logical(kind=1), intent(out), dimension(mo, km) :: lo
+   real, intent(inout), dimension(mo, km) :: go
+   real, intent(inout), dimension(mo) :: rlat
+   real, intent(inout), dimension(mo) :: rlon
+   integer, intent(out) :: iret
 
-! ---------------------------------------------------------------------------------------- 
-! Initialize
-! ---------------------------------------------------------------------------------------- 
-iret=0
-no=0
+   ! --------------------------------------------------------------------------
+   ! Initialize
+   ! --------------------------------------------------------------------------
+   iret = 0
+   no = 0
 
-! ---------------------------------------------------------------------------------------- 
-! Call NCEPLIBS-ip subroutine, ipolates_grib2 to perform interpolation of scalar fields
-! using GRIB2 metadata.
-! ---------------------------------------------------------------------------------------- 
-call ipolates_grib2(ip,ipopt,igdtnumi,igdtmpli,igdtleni,igdtnumo,igdtmplo,igdtleno, &
-                    mi,mo,km,ibi,li,gi,no,rlat,rlon,ibo,lo,go,iret)
+   ! --------------------------------------------------------------------------
+   ! Call NCEPLIBS-ip subroutine, ipolates_grib2 to perform interpolation of
+   ! scalar fields using GRIB2 metadata.
+   ! --------------------------------------------------------------------------
+   call ipolates_grib2(ip, ipopt, igdtnumi, igdtmpli, igdtleni, igdtnumo, &
+                       igdtmplo, igdtleno, mi, mo, km, ibi, li, gi, no, rlat, &
+                       rlon, ibo, lo, go, iret)
 
-return
+   return
 end subroutine interpolate_scalar
-! ---------------------------------------------------------------------------------------- 
-! ---------------------------------------------------------------------------------------- 
-! ---------------------------------------------------------------------------------------- 
-subroutine interpolate_vector(ip,ipopt,igdtnumi,igdtmpli,igdtleni,igdtnumo,igdtmplo,&
-                              igdtleno,mi,mo,km,ibi,li,ui,vi,no,ibo,lo,uo,vo,rlat,rlon,&
-                              crot,srot,iret)
-use ipolatev_mod
-implicit none
+! -----------------------------------------------------------------------------
+! -----------------------------------------------------------------------------
+! -----------------------------------------------------------------------------
+subroutine interpolate_vector(ip, ipopt, igdtnumi, igdtmpli, igdtleni, &
+                              igdtnumo, igdtmplo, igdtleno, mi, mo, km, ibi, &
+                              li, ui, vi, no, ibo, lo, uo, vo, rlat, rlon, &
+                              crot, srot, iret)
+   !f2py threadsafe
+   use ipolatev_mod
+   implicit none
 
-! ---------------------------------------------------------------------------------------- 
-! Input/Output Variables
-! ---------------------------------------------------------------------------------------- 
-integer, intent(in) :: ip
-integer, intent(in), dimension(20) :: ipopt
-integer, intent(in) :: igdtnumi
-integer, intent(in), dimension(igdtleni) :: igdtmpli
-integer, intent(in) :: igdtleni
-integer, intent(in) :: igdtnumo
-integer, intent(in), dimension(igdtleno) :: igdtmplo
-integer, intent(in) :: igdtleno
-integer, intent(in) :: mi
-integer, intent(in) :: mo
-integer, intent(in) :: km
-integer, intent(in), dimension(km) :: ibi
-logical(kind=1), intent(in), dimension(mi,km) :: li
-real, intent(in), dimension(mi,km) :: ui
-real, intent(in), dimension(mi,km) :: vi
-integer, intent(out) :: no
-integer, intent(out), dimension(km) :: ibo
-logical(kind=1), intent(out), dimension(mo,km) :: lo
-real, intent(out), dimension(mo,km) :: uo
-real, intent(out), dimension(mo,km) :: vo
-real, intent(inout), dimension(mo) :: rlat
-real, intent(inout), dimension(mo) :: rlon
-real, intent(inout), dimension(mo) :: crot
-real, intent(inout), dimension(mo) :: srot
-integer, intent(out) :: iret
+   ! --------------------------------------------------------------------------
+   ! Input/Output Variables
+   ! --------------------------------------------------------------------------
+   integer, intent(in) :: ip
+   integer, intent(in), dimension(20) :: ipopt
+   integer, intent(in) :: igdtnumi
+   integer, intent(in) :: igdtleni
+   integer, intent(in), dimension(igdtleni) :: igdtmpli
+   integer, intent(in) :: igdtnumo
+   integer, intent(in) :: igdtleno
+   integer, intent(in), dimension(igdtleno) :: igdtmplo
+   integer, intent(in) :: mi
+   integer, intent(in) :: mo
+   integer, intent(in) :: km
+   integer, intent(in), dimension(km) :: ibi
+   logical(kind=1), intent(in), dimension(mi, km) :: li
+   real, intent(in), dimension(mi, km) :: ui
+   real, intent(in), dimension(mi, km) :: vi
+   integer, intent(out) :: no
+   integer, intent(out), dimension(km) :: ibo
+   logical(kind=1), intent(out), dimension(mo, km) :: lo
+   real, intent(inout), dimension(mo, km) :: uo
+   real, intent(inout), dimension(mo, km) :: vo
+   real, intent(inout), dimension(mo) :: rlat
+   real, intent(inout), dimension(mo) :: rlon
+   real, intent(inout), dimension(mo) :: crot
+   real, intent(inout), dimension(mo) :: srot
+   integer, intent(out) :: iret
 
-! ---------------------------------------------------------------------------------------- 
-! Initialize
-! ---------------------------------------------------------------------------------------- 
-iret=0
-no=0
+   ! --------------------------------------------------------------------------
+   ! Initialize
+   ! --------------------------------------------------------------------------
+   iret = 0
+   no = 0
 
-! ---------------------------------------------------------------------------------------- 
-! Call NCEPLIBS-ip subroutine, ipolatev_grib2 to perform interpolation of vector fields
-! using GRIB2 metadata.
-! ---------------------------------------------------------------------------------------- 
-call ipolatev_grib2(ip,ipopt,igdtnumi,igdtmpli,igdtleni,igdtnumo,igdtmplo,igdtleno,&
-                    mi,mo,km,ibi,li,ui,vi,no,rlat,rlon,crot,srot,ibo,lo,uo,vo,iret)
+   ! --------------------------------------------------------------------------
+   ! Call NCEPLIBS-ip subroutine, ipolatev_grib2 to perform interpolation of
+   ! vector fields using GRIB2 metadata.
+   ! --------------------------------------------------------------------------
+   call ipolatev_grib2(ip, ipopt, igdtnumi, igdtmpli, igdtleni, igdtnumo, &
+                       igdtmplo, igdtleno, mi, mo, km, ibi, li, ui, vi, no, &
+                       rlat, rlon, crot, srot, ibo, lo, uo, vo, iret)
 
-return
+   return
 end subroutine interpolate_vector
